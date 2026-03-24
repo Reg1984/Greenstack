d2ae2cd383ccceab02310fcf9dd870755af82b72
Reg1984 <regorme101@gmail.com>
2026-03-24 22:53:33 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/protected',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
