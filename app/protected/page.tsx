75f800995114e130c6a38ff4b03a546bcef596cb
Reg1984 <regorme101@gmail.com>
2026-03-24 22:23:18 +0000

    Create page.tsx

---
+++ app/protected/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function ProtectedPage() {
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.replace('/')
+  }, [router])
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#030808]">
+      <div className="text-emerald-400">Loading...</div>
+    </div>
+  )
+}
